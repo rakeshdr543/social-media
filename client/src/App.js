c66947cd4aceee07f797def6469ed51c6298f7aa
rakeshdr543 <rakeshdr543@gmail.com>
2020-10-26 21:02:07 +0530

    routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,22 @@
 import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import "semantic-ui-css/semantic.min.css";
+import { Container } from "semantic-ui-react";
+import Home from "./components/Home";
+import Login from "./components/Login";
+import MenuBar from "./components/Menubar";
+import Register from "./components/Register";
 function App() {
-  return <div className='App'>hi world</div>;
+  return (
+    <Router>
+      <Container>
+        <MenuBar />
+        <Route exact path='/' component={Home} />
+        <Route exact path='/login' component={Login} />
+        <Route exact path='/register' component={Register} />
+      </Container>
+    </Router>
+  );
 }
 
 export default App;
--- client/src/index.js
+++ client/src/index.js
@@ -1,9 +1,8 @@
-import React from "react";
 import ReactDOM from "react-dom";
 import ApolloProvider from "./ApolloProvider";
 import reportWebVitals from "./reportWebVitals";
 
-ReactDOM.render(<ApolloProvider />, document.getElementById("root"));
+ReactDOM.render(ApolloProvider, document.getElementById("root"));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
